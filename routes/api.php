f74266269c498879d8cf3b63ceac9a43b0a61bc5
RiccardoSansone <r.sansone@gmail.com>
2023-11-17 16:30:50 +0100

    do milestone 2

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\ProjectController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('projects', function(){
+//     return [
+//         'status'=> 'succes',
+//         'result'=> Project::all(),
+//     ];
+// });
+Route::get('/projects', [ProjectController::class, 'index']);
